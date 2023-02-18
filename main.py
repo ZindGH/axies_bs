4b3c36306d78f076f1a8bd9431dbb6947aeda7a9
zind <zzzRRzzz@mail.ru>
2023-02-18 13:09:57 +0300

    Added function with retrieving active axies team from battle.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
 import json
 from credentials import api_token
+import logging
 
-def get_leaderboard(number_of_places : int=100, first_rank : int=0):
+logging.basicConfig(level=logging.DEBUG, filename='logs.log', format='%(asctime)s :: %(levelname)s :: %(message)s')
+
+
+def get_leaderboard(number_of_places: int = 100, first_rank: int = 0):
     """
 
     :param number_of_places: number of ranks to be returned
@@ -18,9 +22,11 @@ def get_leaderboard(number_of_places : int=100, first_rank : int=0):
         'limit': number_of_places,
         'offset': first_rank
     }
-    r = requests.get(url, headers=headers, params=params )
+    r = requests.get(url, headers=headers, params=params)
     leaders = json.loads(r.text)['_items']
+    logging.debug(f'get_leaderboard\nstatus_code: {r.status_code}')
     return [(leader['userID'], leader['topRank']) for leader in leaders]
+
 
 def get_axie_list(userid: str, number_of_games):
     url = 'https://api-gateway.skymavis.com/x/origin/battle-history'
@@ -35,9 +41,17 @@ def get_axie_list(userid: str, number_of_games):
     }
     r = requests.get(url, headers=headers, params=params)
     battles = json.loads(r.text)['battles']
-    match = 0
     for battle in battles:
-        
-        if match == 3:
-            break
-    return axies
+        if battle['client_ids'][0] == userid:
+            axie_team = battle['first_client_fighters']
+        else:
+            axie_team = battle['second_client_fighters']
+        axie_ids = [axie['axie_id'] for axie in axie_team]
+    logging.info(f'axie_ids: {axie_ids}')
+    return axie_ids
+
+
+if __name__ == '__main__':
+    user_ids = get_leaderboard(first_rank=1000, number_of_places=10)
+    for user_id in user_ids:
+        get_axie_list(number_of_games=3, userid=user_id)
